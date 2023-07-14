c8a4a0ca5781dc5ed3ab5f15cf264588f0e16ab8
admin_neofa <admin@neofa.com>
2023-07-14 08:39:30 +0200

    Added ConnectKit + Wagmi + Viem Connect wallet works

--- src/components/layout/sections/Header.tsx
+++ src/components/layout/sections/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className=" md:py-10 py-7 bg-gotchi-500 flex flex-col md:flex-row gap-y-5 justify-center place-items-center">
       <div className="flex-none lg:w-1/4 md:w-1/6 md:block hidden"></div>  
-      <div className="grow flex justify-center place-items-center md:gap-x-10 gap-x-5 mx-2">
+      <div className="grow flex justify-center place-items-center md:gap-x-10 gap-x-6 mx-2">
         <Link href="/">
           <Image src="/images/logo.png" alt="logo" width={120} height={120} />
         </Link>
